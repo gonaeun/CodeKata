7cb6f6b5594a33de2fca9154d785a2af5a338d96
고나은 <gonaeunn@gmail.com>
2025-03-28 18:53:57 +0900

    Create algorithm14.java

---
+++ src/algorithm14.java
@@ -0,0 +1,20 @@
+public class algorithm14 {
+    // 자릿수 더하기
+
+    public int solution(int n) {
+        int sum = 0;
+        for (int i=1; i<=n; i++) {
+            if (n%i == 0) { // i가 n의 약수인지 확인
+                sum +=i;  // 약수이면 더해라
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // 테스트 출력용
+        int result = new algorithm14().solution(12);
+        System.out.println(result);
+    }
+
+}
